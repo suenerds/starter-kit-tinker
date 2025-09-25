efbaef55e3b0eebdb463737f3aa51fc86a617fb3
Thore Sünert <thoresuenert@users.noreply.github.com>
2025-09-25 17:36:08 +0200

    WIP

--- routes/channels.php
+++ routes/channels.php
@@ -1,8 +1,9 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Support\Facades\Broadcast;
 
-Broadcast::channel('App.Models.User.{id}', function ($user = null, $id) {
+Broadcast::channel('App.Models.User.{id}', function (User $user, $id) {
     return (int) $user->id === (int) $id;
 });
 
